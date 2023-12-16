3726f28883a34f92ff6608f9299acf55f246fea1
amanbhardwaj12072003 <amanbhardwaj12072003@gmail.com>
2023-12-16 10:35:34 +0530

    Static Footer Section Done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from "react";
-import "./style.scss"
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                "Dive into the latest cinematic sensations with our curated selection of trending movies. From gripping dramas to heartwarming comedies, we bring you the hottest releases that are creating waves in the world of cinema. Explore the trends, embrace the excitement, and make every movie night unforgettable with our handpicked favorites."
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
 };
 
 export default Footer;
